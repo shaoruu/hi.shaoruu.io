b6df236aa1be0390849dac056d705adc7ecbf228
Ian Huang (Shaoru) <ian1314159@gmail.com>
2024-02-03 21:47:59 -0800

    fix: fix url

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,9 +1,9 @@
 import { IS_PRODUCTION } from '@/src/utils/secrets';
 
 export function getCoreUrl() {
-  return IS_PRODUCTION ? 'https://shaoruu.io' : 'http://127.0.0.1:4000';
+  return IS_PRODUCTION ? 'https://hi.shaoruu.io' : 'http://127.0.0.1:4000';
 }
 
 export function getServerUrl() {
-  return IS_PRODUCTION ? 'https://server.shaoruu.io' : 'http://localhost:8080';
+  return IS_PRODUCTION ? 'https://hi.shaoruu.io' : 'http://localhost:8080';
 }
