e5236add8ae5fa484a3f30df1c14a08f93231630
Ian Huang (Shaoru) <ian1314159@gmail.com>
2024-02-03 21:53:46 -0800

    fix: fix url

--- core/src/main.rs
+++ core/src/main.rs
@@ -123,7 +123,7 @@ async fn main() -> std::io::Result<()> {
         let cors = Cors::default()
             .allowed_origin("http://localhost:3000")
             .allowed_origin("http://localhost:4000")
-            .allowed_origin("https://shaoruu.io");
+            .allowed_origin("https://hi.shaoruu.io");
 
         let app = App::new()
             .wrap(cors)
