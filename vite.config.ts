3e9bc43ab32703dcc73ab8321ce5733adabd2868
Ian Huang (Shaoru) <ian1314159@gmail.com>
2024-02-11 13:55:22 -0800

    feat: optimize images

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -20,6 +21,6 @@ export default defineConfig(({ mode }) => {
       },
     },
     clearScreen: false,
-    plugins: [react()],
+    plugins: [react(), ViteImageOptimizer()],
   };
 });
